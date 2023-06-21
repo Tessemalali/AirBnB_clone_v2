5e8f44a82b8e6f9fb79abd5bfe40191063dcba83
Tessema <tessemamineo@yahoo.com>
2023-06-20 21:27:16 -0700

    task one

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Generates a .tgz archive from the contents of the web_static folder."""
+"""This generates a .tgz archive from the contents of the web_static folder."""
 from fabric.api import local
 import time
 
 
 def do_pack():
-    """Generate an tgz archive from web_static folder"""
+    """This generates a tgz archive from web_static folder"""
     try:
         local("mkdir -p versions")
         local("tar -cvzf versions/web_static_{}.tgz web_static/".
